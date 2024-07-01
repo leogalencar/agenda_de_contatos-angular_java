3b956a7de5cbe34d83f4ff5f88635ba472f7407b
leogalencar <leonardogalencar444@gmail.com>
2024-07-01 12:17:45 -0300

    Criação do ContactRepository e script SQL teste

---
+++ backend/contact/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_CONTACT (name, email, phone, cellphone, city, gender, birthday, is_Favorite) VALUES
+('Carlos Silva', 'carlos.silva@example.com', '(11) 2345-6789', '(11) 98765-4321', 'São Paulo', 'Masculino', '1985-05-15', true),
+('Ana Pereira', 'ana.pereira@example.com', '(21) 3456-7890', '(21) 91234-5678', 'Rio de Janeiro', 'Feminino', '1992-08-25', false),
+('Bruno Souza', 'bruno.souza@example.com', '(31) 4567-8901', '(31) 92345-6789', 'Belo Horizonte', 'Masculino', '1988-12-10', true);
